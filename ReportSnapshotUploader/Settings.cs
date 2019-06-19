b0638ee3f3f7bb1ccc558bd7be0db458889dc526
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-06-20 00:33:17 +0300

    Report snapshot uploader

---
+++ ReportSnapshotUploader/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReportSnapshotUploader
+{
+    public sealed class Settings
+    {
+        public static readonly IDictionary<string, string> CommandLineMappings = new Dictionary<string, string>
+        {
+            {"-cs", nameof(AzureConnectionString)},
+            {"-f", nameof(FilePath)}
+        };
+
+        public string AzureConnectionString { get; set; }
+        public string FilePath { get; set; }
+        
+        public bool Validate()
+        {
+            if (AzureConnectionString == null)
+            {
+                Console.WriteLine($"'{nameof(AzureConnectionString)}' or command line '-cs' parameter is not specified");
+                return false;
+            }
+
+            if (FilePath == null)
+            {
+                Console.WriteLine($"'{nameof(FilePath)}' or command line '-f' parameter is not specified");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
