7b28333317c1d331784cb397da89a4372a28347b
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2020-03-05 08:52:15 +0300

    JSON-RPC authentication

--- src/Lykke.Job.ChainalysisHistoryExporter/Settings/XrpSettings.cs
+++ src/Lykke.Job.ChainalysisHistoryExporter/Settings/XrpSettings.cs
@@ -5,6 +5,8 @@ namespace Lykke.Job.ChainalysisHistoryExporter.Settings
     public class XrpSettings
     {
         public string RpcUrl { get; set; }
+        public string RpcUsername { get; set; }
+        public string RpcPassword { get; set; }
         public TimeSpan CacheExpirationPeriod { get; set; }
     }
 }
